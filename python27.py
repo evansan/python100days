07eda5c93f0931fb2ba4dc6feaa0600fd349d4b4
noname <2133205+evansan@users.noreply.github.com>
2023-07-16 21:59:53 -0400

    real day 27

--- python27.py
+++ python27.py
@@ -1,1 +1,27 @@
-hello world
+import os, time
+
+def hp():
+    import random
+    hp = random.randint(1,6) * random.randint(1,12) / 2 + 10
+    return hp
+    
+def strength():
+    import random
+    strength = random.randint(1,6) * random.randint(1,8) / 2 + 12
+    return strength
+
+while True:
+    char_name = input("What is the name of your character?: ")
+
+    os.system("clear")
+    print(char_name)
+    time.sleep(1)
+    print("HP: " + str(hp()))
+    time.sleep(1)
+    print("Strength: " + str(strength()))
+    
+    repeat = input("again?: ")
+    if repeat == "yes":
+        os.system("clear")
+    else:
+        break
