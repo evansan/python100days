0fff74d957ab16cfbfb1636d11e842c3142a947a
noname <2133205+evansan@users.noreply.github.com>
2023-07-28 18:20:26 -0400

    day 38

--- python37.py
+++ python37.py
@@ -10,4 +10,12 @@ print("Your Star Wars name is " + f"{(split[0])[0:3] + (split[1])[0:2] + ' ' + (
 
 
 
+# print("STAR WAR NAME GENERATOR\n")
+
+# name = input("""Enter your first name, last name,
+# Mum's maiden name and the
+# city you were born in:\n""").split()
+
+# star_wars_name = (name[0][:3] + name[1][:2] + ' ' + name[2][:2] + name[3][-2:]).title()
+# print(f"Your Star Wars name is {star_wars_name}")
 
--- python38.py
+++ python38.py
@@ -1,15 +1,16 @@
 myString = input("Type something > ")
 
 for letter in myString:
-    if letter.lower() == "b":
+
+    if letter.lower() == "y":
         print('\033[33m', end='')  # yellow
     elif letter.lower() == "g":
         print('\033[32m', end='')  # green
     elif letter.lower() == "p":
         print('\033[35m', end='')  # purple
+    elif letter == " ":
+        print('\033[0m', end='')  # default
 
     print(letter, end="")
-    print('\033[0m', end='')  # back to default
 
-# Reset color at end of string
 print('\033[0m')
